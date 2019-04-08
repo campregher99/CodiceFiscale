31242b5e332d1313e197104a28ae2e8b8a66e45d
Daniele <28843028+Chiaf1@users.noreply.github.com>
2019-04-08 19:08:16 +0200

    Comepletamento GestioneCodici

--- src/nofantasy/codicefiscale2/GestioneCodici.java
+++ src/nofantasy/codicefiscale2/GestioneCodici.java
@@ -1,5 +1,32 @@
 package nofantasy.codicefiscale2;
 
+import java.util.ArrayList;
+
 public class GestioneCodici {
+
+	private ArrayList<Persona> persone = new ArrayList<Persona>();
+	private GestioneXML gestoreXML = new GestioneXML();
+	private ArrayList<String> codiciFiscaliInvalidi = new ArrayList<String>();
+	private ArrayList<String> codiciFiscaliSpaiati = new ArrayList<String>();
+	
+	public GestioneCodici() {
+		codiciFiscaliSpaiati = gestoreXML.letturaCF();
+		
+	}
+	
+	public void aggiungiPersona(Persona persona) {
+		persone.add(persona);
+		convalida()
+	}
+	
+	private boolean controlloCF(String cF) {
+		//serve per controllare che il cf della persona sia all'interno di codici fiscali spaiati
+		return true;
+	}
+	
+	private boolean convalidaCF() {
+		//serve a controllare che i codici inseriti all'inzio in codiciSpaiati siano validi
+		return true;
+	}
 
 }
--- src/nofantasy/codicefiscale2/GestioneXML.java
+++ src/nofantasy/codicefiscale2/GestioneXML.java
@@ -3,37 +3,37 @@ package nofantasy.codicefiscale2;
 import java.util.*;
 
 public class GestioneXML {
-	
+
 	private String pathInputPersone;
 	private String pathComuni;
 	private String pathCF;
-	
-	public String letturaCF(String cF) {
-		return "";
+
+	public ArrayList<String> letturaCF() {
+		return;
 	}
-	
+
 	public String ricercaComune(String comune) {
 		return "";
 	}
-	
+
 	public ArrayList<Persona> letturaPersona() {
 		return;
 	}
-	
+
 	public boolean sethPathInputPersona(String Path) {
 		return true;
 	}
-	
+
 	public boolean sethPathComuni(String path) {
 		return true;
 	}
-	
+
 	public boolean sethPathCF(String path) {
 		return true;
 	}
-	
+
 	public void generaOutput(ArrayList<String> persone, ArrayList<String> cFInvalidi, ArrayList<String> cFSpaiati) {
-		
+
 	}
 
 }
