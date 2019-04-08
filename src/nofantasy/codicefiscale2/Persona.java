5ce0b963668c6b81779d70cafb7bc8e993a968e6
campregher99 <f.campregher@studenti.unibs.it>
2019-04-08 18:55:31 +0200

    costruttori

--- src/nofantasy/codicefiscale2/Persona.java
+++ src/nofantasy/codicefiscale2/Persona.java
@@ -1,22 +1,32 @@
 package nofantasy.codicefiscale2;
 
 public class Persona {
-	
+
 	private String nome;
 	private String cognome;
 	private String sesso;
 	private String comune;
 	private String dataDiNascita;
 	private String cF;
-	
+
 	public Persona(Persona newPersona) {
-		
+		this.nome = newPersona.getNome();
+		this.cognome = newPersona.getCognome();
+		this.sesso = newPersona.getSesso();
+		this.comune = newPersona.getComune();
+		this.dataDiNascita = newPersona.getDataDiNascita();
+		this.cF=calcoloCF();
 	}
-	
+
 	public Persona(String nome, String cognome, String sesso, String comune, String dataDiNascita) {
-		
+		this.nome = nome;
+		this.cognome = cognome;
+		this.sesso = sesso;
+		this.comune = comune;
+		this.dataDiNascita = dataDiNascita;
+		this.cF=calcoloCF();
 	}
-	
+
 	public void setCF(String cF) {
 		this.cF = cF;
 	}
@@ -44,8 +54,9 @@ public class Persona {
 	public String getcF() {
 		return cF;
 	}
-	
+
 	private String calcoloCF() {
+		
 		return "";
 	}
 
