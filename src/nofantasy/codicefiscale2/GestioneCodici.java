7c41156d3d0a761596f5eb77c520a1a063c53f1d
Daniele <28843028+Chiaf1@users.noreply.github.com>
2019-04-08 21:25:19 +0200

    GestioneCodici controlloCF aggiungiPersone costruttore

--- src/nofantasy/codicefiscale2/GestioneCodici.java
+++ src/nofantasy/codicefiscale2/GestioneCodici.java
@@ -11,20 +11,34 @@ public class GestioneCodici {
 	
 	public GestioneCodici() {
 		codiciFiscaliSpaiati = gestoreXML.letturaCF();
-		
+		for (int i = 0; i < codiciFiscaliSpaiati.size(); i++) {
+			if (!(convalidaCF(codiciFiscaliSpaiati.get(i)))) {
+				codiciFiscaliInvalidi.add(codiciFiscaliSpaiati.get(i));
+				codiciFiscaliSpaiati.remove(i);
+			}
+		}
 	}
 	
 	public void aggiungiPersona(Persona persona) {
-		persone.add(persona);
-		convalida()
+		if(!(controlloCF(persona.getcF()))) {
+			persona.setCF("ASSENTE");
+		}else {
+			codiciFiscaliSpaiati.remove(persona.getcF());
+		}
+		persone.add(persona);		
 	}
 	
 	private boolean controlloCF(String cF) {
 		//serve per controllare che il cf della persona sia all'interno di codici fiscali spaiati
-		return true;
+		for (int i = 0; i < codiciFiscaliSpaiati.size(); i++) {!!ma siamno sicuri non serva un <=? perchè non capisco se facendo solo < non controlla l'ultimo
+			if (cF.equals(codiciFiscaliSpaiati.get(i))) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
-	private boolean convalidaCF() {
+	private boolean convalidaCF(String cF) {
 		//serve a controllare che i codici inseriti all'inzio in codiciSpaiati siano validi
 		return true;
 	}
