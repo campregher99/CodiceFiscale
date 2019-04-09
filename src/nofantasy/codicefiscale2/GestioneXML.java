daf7548b099204dd412361199f3c23f1aaa18eec
Daniele <28843028+Chiaf1@users.noreply.github.com>
2019-04-09 20:17:09 +0200

    abbozzo letturaPersone

--- src/nofantasy/codicefiscale2/GestioneXML.java
+++ src/nofantasy/codicefiscale2/GestioneXML.java
@@ -49,9 +49,9 @@ public class GestioneXML {
 					case XMLStreamConstants.CHARACTERS: // content all’interno di un elemento: stampa il testo
 						if (xmlr.getText().trim().length() > 0) // controlla se il testo non contiene solo spazi
 							
-							if (lastTag.equals("codice")) {//se l'ultima tag che ho letto è "codice" prendo il testo
-								codiciLetti.add(xmlr.getText());
-							}
+						if (lastTag.equals("codice")) {//se l'ultima tag che ho letto è "codice" prendo il testo
+							codiciLetti.add(xmlr.getText());
+						}
 						break;
 					}
 					xmlr.next();
@@ -64,9 +64,68 @@ public class GestioneXML {
 	}
 	
 	public void letturaPersona() {
+		String nome = "";
+		String cognome = "";
+		String sesso = "";
+		String comune = "";
+		String data = "";
+		String lastTag = "";
 		
+		try {
+			xmlif = XMLInputFactory.newInstance();
+			xmlr = xmlif.createXMLStreamReader(pathInputPersone, new FileInputStream(pathInputPersone));
+		} catch (Exception e) {
+			System.out.println("Errore nell'inizializzazione del reader:");
+			System.out.println(e.getMessage());
+		}
 		
-		codici.aggiungiPersona(persona);
+		try {
+			while (xmlr.hasNext()) { // continua a leggere finché ha eventi a disposizione
+				switch (xmlr.getEventType()) { // switch sul tipo di evento
+					case XMLStreamConstants.START_DOCUMENT: // inizio del documento 
+						
+						break;
+					case XMLStreamConstants.START_ELEMENT:
+						
+						lastTag = xmlr.getLocalName();//passo ad una variabile d'appoggio l'ultima tag che ho letto
+						
+						break;
+					case XMLStreamConstants.END_ELEMENT: // fine di un elemento 
+						if (xmlr.getLocalName().equals("nome")) {
+							Persona newPersona = new Persona(nome, cognome, sesso, comune, ricercaComune(comune), data);
+							codici.aggiungiPersona(newPersona);
+						}
+						break;
+					case XMLStreamConstants.COMMENT:
+						
+						break;  
+					case XMLStreamConstants.CHARACTERS: // content all’interno di un elemento: stampa il testo
+						if (xmlr.getText().trim().length() > 0) // controlla se il testo non contiene solo spazi
+							
+						if (lastTag.equals("nome")) {//se l'ultima tag che ho letto è "codice" prendo il testo
+							nome = xmlr.getText();
+						}
+						if (lastTag.equals("cognome")) {//se l'ultima tag che ho letto è "codice" prendo il testo
+							cognome = xmlr.getText();
+						}	
+						if (lastTag.equals("sesso")) {//se l'ultima tag che ho letto è "codice" prendo il testo
+							sesso = xmlr.getText();
+						}
+						if (lastTag.equals("comune_nascita")) {//se l'ultima tag che ho letto è "codice" prendo il testo
+							comune = xmlr.getText();
+						}
+						if (lastTag.equals("data_nascita")) {//se l'ultima tag che ho letto è "codice" prendo il testo
+							data = xmlr.getText();
+						}
+						
+						break;
+					}
+					xmlr.next();
+				}
+		}catch (Exception e){
+			System.out.println("Errore durante la lettura del file: ");
+			System.out.println(e.getMessage());
+		}
 	}
 
 	private String ricercaComune(String comune) {
