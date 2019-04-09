98bc34fe1afa50a82c00ee8550220b358b30f38f
campregher99 <f.campregher@studenti.unibs.it>
2019-04-09 18:08:58 +0200

    lettura cf

--- src/nofantasy/codicefiscale2/GestioneXML.java
+++ src/nofantasy/codicefiscale2/GestioneXML.java
@@ -2,35 +2,105 @@ package nofantasy.codicefiscale2;
 
 import java.util.*;
 
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamReader;
+
+import java.io.FileInputStream;
+
+
 public class GestioneXML {
 
 	private String pathInputPersone;
 	private String pathComuni;
 	private String pathCF;
-	private GestioneCodici codici;
+	private GestioneCodici codici;		
+	private XMLInputFactory xmlif = null;
+	private XMLStreamReader xmlr = null;
 
 	public void letturaCF() {
+		ArrayList<String> codiciLetti = new ArrayList<String>();
+		try {
+			xmlif = XMLInputFactory.newInstance();
+			xmlr = xmlif.createXMLStreamReader(pathCF, new FileInputStream(pathCF));
+		} catch (Exception e) {
+			System.out.println("Errore nell'inizializzazione del reader:");
+			System.out.println(e.getMessage());
+		}
 		
-		codici = new GestioneCodici(newCodici);
+		try {
+			while (xmlr.hasNext()) { // continua a leggere finché ha eventi a disposizione
+				switch (xmlr.getEventType()) { // switch sul tipo di evento
+					case XMLStreamConstants.START_DOCUMENT: // inizio del documento: stampa che inizia il documento
+						
+						break;
+					case XMLStreamConstants.START_ELEMENT:
+						for (int i = 0; i < xmlr.getAttributeCount(); i++)
+							if("codice".equals(xmlr.getAttributeLocalName(i))) {
+								codiciLetti.add(xmlr.getAttributeValue(i));
+							}
+						break;
+					case XMLStreamConstants.END_ELEMENT: // fine di un elemento: stampa il nome del tag chiuso
+						System.out.println("END-Tag " + xmlr.getLocalName()); 
+						break;
+					case XMLStreamConstants.COMMENT:
+						System.out.println("// commento " + xmlr.getText()); 
+						break; // commento: ne stampa il contenuto
+					case XMLStreamConstants.CHARACTERS: // content all’interno di un elemento: stampa il testo
+						if (xmlr.getText().trim().length() > 0) // controlla se il testo non contiene solo spazi
+							System.out.println("-> " + xmlr.getText());
+						break;
+					}
+					xmlr.next();
+				}
+		}catch (Exception e){
+			System.out.println("Errore durante la lettura del file: ");
+			System.out.println(e.getMessage());
+		}
+		codici = new GestioneCodici(codiciLetti);
 	}
-
-	public String ricercaComune(String comune) {
-		return "";
-	}
-
+	
 	public void letturaPersona() {
 		codici.aggiungiPersona(persona);
 	}
 
-	public boolean sethPathInputPersona(String Path) {
+	private String ricercaComune(String comune) {
+		return "";
+	}
+
+	public boolean sethPathInputPersona(String path) {
+		try {
+			xmlif = XMLInputFactory.newInstance();
+			xmlr = xmlif.createXMLStreamReader(path, new FileInputStream(path));
+			pathInputPersone = path;
+		} catch (Exception e) {
+			System.out.println("Non è stato possibile trovare il file");
+			System.out.println(e.getMessage());
+		}
 		return true;
 	}
 
 	public boolean sethPathComuni(String path) {
+		try {
+			xmlif = XMLInputFactory.newInstance();
+			xmlr = xmlif.createXMLStreamReader(path, new FileInputStream(path));
+			pathComuni = path;
+		} catch (Exception e) {
+			System.out.println("Non è stato possibile trovare il file");
+			System.out.println(e.getMessage());
+		}
 		return true;
 	}
 
 	public boolean sethPathCF(String path) {
+		try {
+			xmlif = XMLInputFactory.newInstance();
+			xmlr = xmlif.createXMLStreamReader(path, new FileInputStream(path));
+			pathCF = path;
+		} catch (Exception e) {
+			System.out.println("Non è stato possibile trovare il file");
+			System.out.println(e.getMessage());
+		}
 		return true;
 	}
 
